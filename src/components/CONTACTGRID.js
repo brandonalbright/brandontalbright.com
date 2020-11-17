16062236e8344235bc89f93f772b16ed5b3e631e
Brandon Albright <brandon4692@gmail.com>
2020-11-17 09:34:21 -0600

    add modal, add contacts

---
+++ src/components/CONTACTGRID.js
@@ -0,0 +1,8 @@
+import React from  "react";
+
+function Contacts(props) {
+    return <h1>These are Contacts</h1>
+}
+
+export default Contacts
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import React from  "react";
 import ReactDOM from "react-dom";
+import ContactModal from "./components/MODAL"
+import Contacts from "./components/CONTACTGRID"
+import fetchAPI from "./api/index"
+
+
+function App() {
+    
+    fetchAPI("https://univ-contact-book.herokuapp.com/api/contacts")
+        .then(function (data) {
+        console.log('my contacts', data);
+        })
+        .catch(function (error) {
+        console.error('error fetching contacts', error);
+        })
+    
+    return (
+        <>
+            <h1>Hello!</h1>
+            <ContactModal />
+            <Contacts />
+        </>
+    )
+}
 
 ReactDOM.render(
-    <h1>Hello, World!</h1>,
+    <App />,
     document.getElementById(`app`)
 );
